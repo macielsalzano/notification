ec74f504bcf9c242b28eb25fb39c5dfbf26c2aed
Maciel Salzano <macielsalzano@gmail.com>
2021-06-09 11:33:15 -0300

    Test fix

--- web/index.php
+++ web/index.php
@@ -4,9 +4,10 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email("SmtpDebug", "yourHost", "yourUser", "yourPassword", "tls", 'yourPort',
-    "your@email.com", "yourName");
-$novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>test</b></p>", "your@email.com", "yourName", "address@email.com", "name of who will receive");
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls/ssl)", "port (587)",
+    "from@email.com", "From Name");
+
+$email->sendEmail("Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 
 var_dump($novoEmail);
 
